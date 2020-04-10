049a136841677d147f5cc72fcee582c31822190c
kannan-16 <mailtokannan16@gmail.com>
2020-04-10 19:46:50 +0530

    modif

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,25 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, IOException, SQLException {
 		
-	// Fill your code
+		Connection con = null;
 		
+		ConnectionManager cm = new ConnectionManager();
+		
+		con = cm.getConnection();
+		
+		if(con!=null) {
+			System.out.println("Established");
+		}
+		else {
+			System.out.println("Not Established");			
+		}
 	}
 }
